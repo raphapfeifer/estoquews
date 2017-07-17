662881b408d29d1ea2928e1f95e3bc5c869c3d97
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2017-07-17 01:23:16 -0300

    17/07/2017

---
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -0,0 +1,26 @@
+package br.com.caelum.estoque.ws;
+
+
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import br.com.caelum.estoque.modelo.item.Item;
+import br.com.caelum.estoque.modelo.item.ItemDao;
+
+@WebService
+public class EstoqueWS {
+
+	private ItemDao dao = new ItemDao();
+	
+	
+	public List<Item> getItens(){
+		
+		System.out.println("Chamando getItens()");
+		List<Item> lista = dao.todosItens();
+		return lista;
+	}
+	
+	
+}
